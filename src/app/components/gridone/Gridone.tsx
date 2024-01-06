29289534fc8130873042b13c0e129ece922da045
hassan27one <143028617+hassan27one@users.noreply.github.com>
2024-01-06 21:17:08 +0500

    commit-1

---
+++ src/app/components/gridone/Gridone.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Gridone() {
+  return (
+    <>
+    <section >
+    <h1 className='title'> Case Studies</h1>
+    <div className='wrapper'>
+
+    </div>
+
+    </section>
+    
+    </>
+  )
+}
